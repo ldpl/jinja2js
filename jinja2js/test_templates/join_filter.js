732cfb79f201dc4be8d7a380a8a55cb2f9d5ee7f
Pavel Stupnikov <dp@dpointer.org>
2012-11-08 03:38:06 +0400

    run generated js for tests and compare output

--- jinja2js/test_templates/join_filter.js
+++ jinja2js/test_templates/join_filter.js
@@ -6,7 +6,7 @@ __ns.join_filter_default = function() {
     return __output;
 };
 
-__ns.join_filter = function() {
+__ns.join_filter_comma = function() {
     var __data = _.parse_args(arguments, []);
     var __output = '';
     __output += _.escape(['a', 'b', 'c'].join(','));
@@ -31,6 +31,13 @@ __ns.join_filter_complex_arg = function() {
     var __data = _.parse_args(arguments, ['config', 'type']);
     var __output = '';
     __output += _.escape(['a', 'b', 'c'].join(__data.config.separator[__data.type].value));
+    return __output;
+};
+
+__ns.join_filter = function() {
+    var __data = _.parse_args(arguments, ['value', 'separator']);
+    var __output = '';
+    __output += _.escape(__data.value.join(__data.separator));
     return __output;
 };
 })(window.jinja2js = window.jinja2js || {}, jinja2support);
--- jinja2js/tests.py
+++ jinja2js/tests.py
@@ -4,6 +4,7 @@ import os
 import re
 
 from nose.tools import raises
+import spidermonkey
 
 from jinja2 import Environment, PackageLoader
 from jinja2.compiler import TemplateAssertionError
@@ -33,6 +34,39 @@ def load_and_compare(source_file, expected_file):
         compare(src, expected)
 
 
+def execute_js(source_file, tests_file):
+    tests = []
+    for l in open(tests_file):
+        l = l.strip()
+        if l.startswith('>'):
+            content = l[1:].strip()
+            tests.append([content, None])
+        elif l.startswith('<'):
+            content = l[1:].strip()
+            if tests[-1][1] is None:
+                tests[-1][1] = content
+            else:
+                tests[-1][1] += '\n' + content
+    rt = spidermonkey.Runtime()
+    cx = rt.new_context()
+    window = {}
+    cx.add_global('window', window)
+    support_js = open('../js/jinja2support.js').read()
+    cx.execute(support_js)
+    cx.add_global('jinja2support', window['jinja2support'])
+    src = jscompiler.generate(env, None, source_file)
+    cx.execute(src)
+    for command, expected in tests:
+        result = cx.execute('window.jinja2js.' + command).strip()
+        if result != expected:
+            print "Test:", command
+            print "Expected:"
+            print expected
+            print "Result:"
+            print result
+            assert False, "Test failed"
+
+
 def test_file_templates():
     # test will run from either location
     directory = 'jinja2js/test_templates'
@@ -44,7 +78,18 @@ def test_file_templates():
 
     for f in files:
         js_file = os.path.join(directory, re.sub('\\.jinja$', '.js', f))
-        yield load_and_compare, f, js_file
+        correct_test = False
+        if os.path.exists(js_file):
+            correct_test = True
+            yield load_and_compare, f, js_file
+        test_file = os.path.join(directory, re.sub('\\.jinja$', '.test', f))
+        if os.path.exists(test_file):
+            correct_test = True
+            yield execute_js, f, test_file
+        if not correct_test:
+            def fail():
+                assert False, "Invalid test: .js or .test file required"
+            yield fail
 
 
 @raises(TemplateAssertionError)
